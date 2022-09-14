9d4d28f70d9f1d128c30d0fd3a6a4932c96fa814
Hardik Bansal <96608629+bansal-hardik@users.noreply.github.com>
2022-09-14 20:35:52 +0530

    Rename Exercise1.sql to Assignment/Exercise1.sql

--- Assignment/Exercise1.sql
+++ Assignment/Exercise1.sql
@@ -3,7 +3,7 @@ SELECT COUNT(*) AS 'NumberOfRecords'
 FROM Sales.SalesPerson;
 
 --2.	Select both the FirstName and LastName of records from the Person table 
---      where the FirstName begins with the letter ‘B’. (Schema(s) involved: Person)
+--      where the FirstName begins with the letter â€˜Bâ€™. (Schema(s) involved: Person)
 
 SELECT FirstName ,LastName
 FROM Person.Person 
@@ -35,8 +35,8 @@ FROM Production.Product ORDER BY [Weight] DESC;
 SELECT Description,COALESCE(MaxQty,0.00)
 FROM Sales.SpecialOffer;
 
---6.	Display the overall Average of the [CurrencyRate].[AverageRate] values for the exchange rate ‘USD’ to ‘GBP’
---		for the year 2005 i.e. FromCurrencyCode = ‘USD’ and ToCurrencyCode = ‘GBP’.
+--6.	Display the overall Average of the [CurrencyRate].[AverageRate] values for the exchange rate â€˜USDâ€™ to â€˜GBPâ€™
+--		for the year 2005 i.e. FromCurrencyCode = â€˜USDâ€™ and ToCurrencyCode = â€˜GBPâ€™.
 --		Note: The field [CurrencyRate].[AverageRate] is defined as 'Average exchange rate for the day.' 
 --		(Schema(s) involved: Sales)
 
@@ -46,7 +46,7 @@ WHERE FromCurrencyCode='USD'
 AND ToCurrencyCode='GBP'
 AND YEAR(CurrencyRateDate)=2005;
 
---7.	Display the FirstName and LastName of records from the Person table where FirstName contains the letters ‘ss’. 
+--7.	Display the FirstName and LastName of records from the Person table where FirstName contains the letters â€˜ssâ€™. 
 --		Display an additional column with sequential numbers for each row returned beginning at integer 1. (Schema(s) involved: Person)
 
 SELECT FirstName +' '+LastName AS FullName,
@@ -62,7 +62,7 @@ WHERE FirstName LIKE '%ss%';
 --			Up To 1%	Band 1
 --			Up To 1.5%	Band 2
 --		Greater 1.5%	Band 3
---		Display the [SalesPersonID] with an additional column entitled ‘Commission Band’ indicating the appropriate band as above.
+--		Display the [SalesPersonID] with an additional column entitled â€˜Commission Bandâ€™ indicating the appropriate band as above.
 
 
 SELECT BusinessEntityID,
@@ -73,7 +73,7 @@ ELSE 'BAND 3'
 END AS 'Commmision Band'
 FROM Sales.SalesPerson;
 
---9.	Display the managerial hierarchy from Ruth Ellerbrock (person type – EM) up to CEO Ken Sanchez.
+--9.	Display the managerial hierarchy from Ruth Ellerbrock (person type â€“ EM) up to CEO Ken Sanchez.
 --		Hint: use [uspGetEmployeeManagers] (Schema(s) involved: [Person], [HumanResources]) 
 
 DECLARE @ID int;
